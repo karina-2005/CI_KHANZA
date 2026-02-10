c1a800d24394c825159054a8653ecf40b3d4387e
Khanza Afifah <pkhanza25@gmail.com>
2026-02-10 15:49:16 +0700

    latihan2
    menambahkan function

--- application/controllers/hello.php
+++ application/controllers/hello.php
@@ -2,6 +2,17 @@
 
 class hello extends CI_Controller {
     
+    public function index ()
+{
+    $data['nama'] = "si cantik";
+    $this->load->view('hello_view',$data);
+}
+    public function namasaya()
+    
 
+{
+    $data['nama'] = "m";
+   $this->load->view('hello_view',$data);
 }
 
+}
